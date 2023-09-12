1b69209147f033b4ef9d06788dc2ac85b7c24323
Skywalker <imsho0218@gmail.com>
2023-09-12 18:02:28 +0800

    test

--- mihoyo_claim.py
+++ mihoyo_claim.py
@@ -13,20 +13,20 @@ options.add_argument('--headless=new')
 driver = webdriver.Chrome(options=options)
 driver.get("https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&mhy_auth_required=true&mhy_presentation_style=fullscreen&lang=zh-tw&bbs_theme=dark&bbs_theme_device=1")
 time.sleep(3)
-# cookies = pickle.load(open("mihoyo.pkl", "rb"))
-# for cookie in cookies:
-#     driver.add_cookie(cookie)
-# driver.refresh()
-# time.sleep(3)
+cookies = pickle.load(open("mihoyo.pkl", "rb"))
+for cookie in cookies:
+    driver.add_cookie(cookie)
+driver.refresh()
+time.sleep(3)
 
 driver.find_element(By.XPATH, "//*[@class='components-home-assets-__sign-guide_---guide-close---2VvmzE']").click()
-more = driver.find_element(By.XPATH, "//*[@class='components-home-assets-__sign-content-test_---more-icon---202NrS']").click()
+driver.find_element(By.XPATH, "//*[@class='components-home-assets-__sign-content-test_---more-icon---202NrS']").click()
 time.sleep(3)
 try:
     reward = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@class='components-home-assets-__sign-content-test_---sign-item---3gtMqV components-home-assets-__sign-content-test_---sign-wrapper---22GpLY']")))
-    # actions = ActionChains(driver)
-    # actions.move_to_element(reward).click().perform()
-    reward.click()
+    actions = ActionChains(driver)
+    actions.move_to_element(reward).click().perform()
+    # reward.click()
 except NoSuchElementException:
     print("Already claimed")
     pass
